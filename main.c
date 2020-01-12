6530c2fa5d1180c9bd144f54fe6b921b1037e216
jbejaoui <jbejaoui@kaliop.com>
2020-01-12 22:39:56 +0100

    Add commands response system.

--- main.c
+++ main.c
@@ -12,12 +12,17 @@
 #define PORT 32456
 
 #define CLIENTS_MAX_COUNT 20
+#define COMMAND_BUFFER_MAX_LENGTH 256
+#define MESSAGE_BUFFER_MAX_LENGTH 2048
+
+#define FILE_LISTING_COMMAND "ls -A"
 
 typedef struct Client {
 	int index;
 	struct sockaddr_in address;
 	socklen_t addressLength;
 	int socketDescriptor;
+	pthread_t communicationThread;
 } Client;
 
 typedef struct Gateway {
@@ -25,6 +30,15 @@ typedef struct Gateway {
 	int clientsCount;
 	Client* clients[CLIENTS_MAX_COUNT];
 } Gateway;
+
+typedef struct MessageTransmissionParams {
+    Client* senderClient;
+    Gateway* gateway;
+} MessageTransmissionParams;
+
+void* t_communication();
+
+void _executeCommand(char* cmd, char* buffer);
 
 int main() {
 	/* Initialize the server side socket */
@@ -57,6 +71,8 @@ int main() {
 
     /* Make the gateway between both clients to make the communication works */
     int i;
+    int resThreadCreation;
+    MessageTransmissionParams* communicationParams;
 
     while(1) {
         if(gateway.clientsCount != CLIENTS_MAX_COUNT) {
@@ -72,12 +88,15 @@ int main() {
 
             printf("New connection\n");
 
-            /* Useful in the case where clients count value
-             * has been changed during the accepting process. */
-            gateway.clients[gateway.clientsCount] = gateway.clients[i];
-            i = gateway.clientsCount;
-            gateway.clients[i]->index = i;
-            /********************/
+            communicationParams = (MessageTransmissionParams*) malloc(sizeof(MessageTransmissionParams));
+            communicationParams->senderClient = gateway.clients[i];
+            communicationParams->gateway = &gateway;
+
+            resThreadCreation = pthread_create(&gateway.clients[i]->communicationThread, NULL, (void*) &t_communication, communicationParams);
+
+            if(resThreadCreation != 0) {
+                perror("Thread Creation Error");
+            }
 
             gateway.clientsCount++;
             /********************/
@@ -92,5 +111,61 @@ int main() {
     /********************/
 
 	return 0;
+}
+
+void* t_communication(MessageTransmissionParams* params) {
+    int resSend, resRecv;
+    int isGatewayEstablished = 1;
+    char commandBuffer[COMMAND_BUFFER_MAX_LENGTH];
+    char messageBuffer[MESSAGE_BUFFER_MAX_LENGTH];
+
+    while(isGatewayEstablished == 1) {
+        if((resRecv = recv(params->senderClient->socketDescriptor, &commandBuffer, sizeof(char)*COMMAND_BUFFER_MAX_LENGTH, 0)) == -1) {
+            perror("Command Reception Error");
+            isGatewayEstablished = 0;
+        }
+        else if(resRecv == 0) {
+            perror("Client Down");
+            isGatewayEstablished = 0;
+        }
+        else {
+            if(strcmp(commandBuffer, "rls") == 0) {
+                char* cmd = FILE_LISTING_COMMAND;
+
+                _executeCommand(cmd, commandBuffer);
+            }
+
+            strcpy(messageBuffer, commandBuffer);
+
+            if((resSend = send(params->senderClient->socketDescriptor, &messageBuffer, (MESSAGE_BUFFER_MAX_LENGTH*sizeof(char)), 0)) == -1) {
+                perror("Nickname Feedback Sending Error");
+                isGatewayEstablished = 0;
+            }
+            if(resSend == 0) {
+                perror("Client Down");
+                isGatewayEstablished = 0;
+            }
+        }
+    }
+}
+
+void _executeCommand(char* cmd, char* buffer) {
+    FILE *pFile;
+
+    if((pFile = popen(cmd, "r")) == NULL) {
+        perror("Pipe Opening Error");
+        return;
+    }
+
+    char filename[COMMAND_BUFFER_MAX_LENGTH];
+
+    while(fgets(filename, COMMAND_BUFFER_MAX_LENGTH, pFile) != NULL) {
+        strcat(buffer, filename);
+    }
+
+    if(pclose(pFile))  {
+        perror("Pipe Closing Error");
+        return;
+    }
 }
 
